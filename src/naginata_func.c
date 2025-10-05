eab71a244d4213445a37c48d868dfd3e368e0243
hamehame2 <38934289+hamehame2@users.noreply.github.com>
2025-10-05 19:35:02 +0900

    Update naginata_func.ckakkobara2

--- src/naginata_func.c
+++ src/naginata_func.c
@@ -266,9 +266,9 @@ void ngh_JKS() { // 『{改行}
     raise_zmk_keycode_state_changed_from_encoded(LS(N9), false, timestamp);
     k_msleep(20);    
     raise_zmk_keycode_state_changed_from_encoded(ENTER, true, timestamp);
-    k_msleep(10);
-    raise_zmk_keycode_state_changed_from_encoded(ENTER, false, timestamp);
     k_msleep(40);
+    raise_zmk_keycode_state_changed_from_encoded(ENTER, false, timestamp);
+    k_msleep(50);
     raise_zmk_keycode_state_changed_from_encoded(LEFT, true, timestamp);
     raise_zmk_keycode_state_changed_from_encoded(LEFT, false, timestamp);
 }
