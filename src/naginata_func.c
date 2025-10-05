2b155cf94c39da23000f1fe9490ca70557c6a4d6
hamehame2 <38934289+hamehame2@users.noreply.github.com>
2025-10-05 19:26:30 +0900

    Update naginata_func.c_kakkodelaybara

--- src/naginata_func.c
+++ src/naginata_func.c
@@ -254,17 +254,21 @@ void ngh_JKS() { // 『{改行}
     //raise_zmk_keycode_state_changed_from_encoded(LEFT, true, timestamp);
     //raise_zmk_keycode_state_changed_from_encoded(LEFT, false, timestamp);
     raise_zmk_keycode_state_changed_from_encoded(LS(N8), true, timestamp);
+    k_msleep(10);
     raise_zmk_keycode_state_changed_from_encoded(LS(N8), false, timestamp);
+    k_msleep(10);
     raise_zmk_keycode_state_changed_from_encoded(ENTER, true, timestamp);
+    k_msleep(10);
     raise_zmk_keycode_state_changed_from_encoded(ENTER, false, timestamp);
-    k_msleep(50);
+    k_msleep(20);
     raise_zmk_keycode_state_changed_from_encoded(LS(N9), true, timestamp);
+    k_msleep(10); 
     raise_zmk_keycode_state_changed_from_encoded(LS(N9), false, timestamp);
-    k_msleep(50);    
+    k_msleep(20);    
     raise_zmk_keycode_state_changed_from_encoded(ENTER, true, timestamp);
-    //k_msleep(150);
+    k_msleep(10);
     raise_zmk_keycode_state_changed_from_encoded(ENTER, false, timestamp);
-    k_msleep(100);
+    k_msleep(40);
     raise_zmk_keycode_state_changed_from_encoded(LEFT, true, timestamp);
     raise_zmk_keycode_state_changed_from_encoded(LEFT, false, timestamp);
 }
